6a9d0738098868fcaddd17730c3f1ca9b37b90ff
Sougata Mondal <sougataofficial362000@gmail.com>
2023-12-23 14:45:33 +0530

    backend update

---
+++ backend/api/product.js
@@ -0,0 +1,102 @@
+const dummyProducts = [
+  {
+    id: 1,
+    model: "Apple iPhone 14 Pro",
+    image:
+      "https://img.freepik.com/free-photo/smartphone-with-blank-black-screen-innovative-future-technology_53876-97896.jpg?w=740&t=st=1703278639~exp=1703279239~hmac=d35a1e4f8560ec64d77e0c306aa1e226e6751e4e480890e2669d4fa23bdcf497",
+    price: "1199.00",
+    processor: "A16 Bionic",
+    os: "iOS",
+  },
+  {
+    id: 2,
+    model: "Samsung Galaxy S23 Ultra",
+    image:
+      "https://img.freepik.com/free-photo/smartphone-with-blank-black-screen-innovative-future-technology_53876-97896.jpg?w=740&t=st=1703278639~exp=1703279239~hmac=d35a1e4f8560ec64d77e0c306aa1e226e6751e4e480890e2669d4fa23bdcf497",
+    price: "1299.00",
+    processor: "Snapdragon 8 Gen 2",
+    os: "Android",
+  },
+  {
+    id: 3,
+    model: "Google Pixel 8",
+    image:
+      "https://img.freepik.com/free-photo/smartphone-with-blank-black-screen-innovative-future-technology_53876-97896.jpg?w=740&t=st=1703278639~exp=1703279239~hmac=d35a1e4f8560ec64d77e0c306aa1e226e6751e4e480890e2669d4fa23bdcf497",
+    price: "899.00",
+    processor: "Tensor 3",
+    os: "Android",
+  },
+  {
+    id: 4,
+    model: "OnePlus 11 Pro",
+    image:
+      "https://img.freepik.com/free-photo/smartphone-with-blank-black-screen-innovative-future-technology_53876-97896.jpg?w=740&t=st=1703278639~exp=1703279239~hmac=d35a1e4f8560ec64d77e0c306aa1e226e6751e4e480890e2669d4fa23bdcf497",
+    price: "999.00",
+    processor: "Snapdragon 8 Gen 2",
+    os: "Android",
+  },
+  {
+    id: 5,
+    model: "Xiaomi 14 Ultra",
+    image:
+      "https://img.freepik.com/free-photo/smartphone-with-blank-black-screen-innovative-future-technology_53876-97896.jpg?w=740&t=st=1703278639~exp=1703279239~hmac=d35a1e4f8560ec64d77e0c306aa1e226e6751e4e480890e2669d4fa23bdcf497",
+    price: "799.00",
+    processor: "Snapdragon 8 Gen 2 Plus",
+    os: "Android",
+  },
+  {
+    id: 6,
+    model: "OPPO Find X6 Pro",
+    image:
+      "https://img.freepik.com/free-photo/smartphone-with-blank-black-screen-innovative-future-technology_53876-97896.jpg?w=740&t=st=1703278639~exp=1703279239~hmac=d35a1e4f8560ec64d77e0c306aa1e226e6751e4e480890e2669d4fa23bdcf497",
+    price: "1099.00",
+    processor: "Snapdragon 8 Gen 1 Plus",
+    os: "Android",
+  },
+  {
+    id: 7,
+    model: "Vivo X90 Pro+",
+    image:
+      "https://img.freepik.com/free-photo/smartphone-with-blank-black-screen-innovative-future-technology_53876-97896.jpg?w=740&t=st=1703278639~exp=1703279239~hmac=d35a1e4f8560ec64d77e0c306aa1e226e6751e4e480890e2669d4fa23bdcf497",
+    price: "899.00",
+    processor: "MediaTek Dimensity 9000+",
+    os: "Android",
+  },
+  {
+    id: 8,
+    model: "Motorola Edge 40 Pro",
+    image:
+      "https://img.freepik.com/free-photo/smartphone-with-blank-black-screen-innovative-future-technology_53876-97896.jpg?w=740&t=st=1703278639~exp=1703279239~hmac=d35a1e4f8560ec64d77e0c306aa1e226e6751e4e480890e2669d4fa23bdcf497",
+    price: "699.00",
+    processor: "Snapdragon 8 Gen 1",
+    os: "Android",
+  },
+  {
+    id: 9,
+    model: "Sony Xperia 1 V",
+    image:
+      "https://img.freepik.com/free-photo/smartphone-with-blank-black-screen-innovative-future-technology_53876-97896.jpg?w=740&t=st=1703278639~exp=1703279239~hmac=d35a1e4f8560ec64d77e0c306aa1e226e6751e4e480890e2669d4fa23bdcf497",
+    price: "1399.00",
+    processor: "Snapdragon 8 Gen 2",
+    os: "Android",
+  },
+  {
+    id: 10,
+    model: "Nokia X23 Pro",
+    image:
+      "https://img.freepik.com/free-photo/smartphone-with-blank-black-screen-innovative-future-technology_53876-97896.jpg?w=740&t=st=1703278639~exp=1703279239~hmac=d35a1e4f8560ec64d77e0c306aa1e226e6751e4e480890e2669d4fa23bdcf497",
+    price: "749.00",
+    processor: "Snapdragon 8 Gen 1",
+    os: "Android",
+  },
+  {
+    id: 11,
+    model: "Asus ROG Phone 7",
+    image:
+      "https://img.freepik.com/free-photo/smartphone-with-blank-black-screen-innovative-future-technology_53876-97896.jpg?w=740&t=st=1703278639~exp=1703279239~hmac=d35a1e4f8560ec64d77e0c306aa1e226e6751e4e480890e2669d4fa23bdcf497",
+    price: "1099.00",
+    processor: "Snapdragon 8 Gen 2",
+  },
+];
+
+module.exports = dummyProducts;
--- backend/index.js
+++ backend/index.js
@@ -4,20 +4,28 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
+const allProducts = require("./api/product");
 dotenv.config();
 app.use(cors());
 app.use(express.json());
 
-
-
 mongoose
-  .connect(process.env.MONGO_URL, {
-  })
+  .connect(process.env.MONGO_URL, {})
   .then(console.log("connected to mongodb!!!"))
   .catch((err) => console.log(err));
 
-app.get('/', (req,res) => {
-    res.send('server is running');
+app.get("/", (req, res) => {
+  console.log(req);
+  res.send("server is running");
+});
+
+app.get("/api/dummyproducts", async (req, res) => {
+  try {
+    res.send(allProducts);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching products");
+  }
 });
 
 app.use("/server/auth", authRoute);
@@ -25,6 +33,5 @@ app.use("/server/auth", authRoute);
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Serve at http://localhost:${port}`)
-    }
-);
+  console.log(`Serve at http://localhost:${port}`);
+});
